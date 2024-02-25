982e6c065f3449e877f6f87622076ffa8db8cd01
Andres Caicedo <andrezz1997@gmail.com>
2024-02-25 23:02:05 +0100

    fix: Refactor CSS styling in App.vue

--- src/App.vue
+++ src/App.vue
@@ -16,25 +16,25 @@ export default {
 </script>
 
 <style>
-*:root{
-	--primary-color		: black;
-	--secondary-color	: white;
-	--tertiary-color	: gray;
+*:root {
+  --primary-color: black;
+  --secondary-color: white;
+  --tertiary-color: gray;
 }
 
-body{
-	background-color: var( --primary-color );
-	margin			: 0;
-	padding			: 0;
+body {
+  background-color: var(--primary-color);
+  margin: 0;
+  padding: 0;
 }
 
 #app {
-  font-family				: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing	: antialiased;
-  -moz-osx-font-smoothing	: grayscale;
-  text-align				: center;
-  background-color			: var( --primary-color );
-  color						: var( --secondary-color );
-  margin					: 0px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  background-color: var(--primary-color);
+  color: var(--secondary-color);
+  margin: 0px;
 }
 </style>
