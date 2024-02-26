0dc8279b2d655e5bb02ba539473882dcf0423f77
Andres Caicedo <andrezz1997@gmail.com>
2024-02-26 11:35:06 +0100

    fix: Refactor job listing component in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,45 @@
 <template>
-  <div id="jobs">
-    <JobsC />
-  </div>
+	<div id="main-content">
+		<ul>
+			<JobListing
+				v-for="(job, index) in jobs"
+				:key="index"
+				:icon="job.icon"
+				:name="job.name"
+				:requirements="job.requirements"
+				:company="job.company"
+				:location="job.location"
+				:salary="job.salary"
+				:vacants="job.vacants"
+				:creation-date="job.creationDate"
+			/>
+		</ul>
+	</div>
 </template>
 
 <script>
-import JobsC from './components/JobsC.vue'
+import JobListing from './components/JobListing.vue'
 
 export default {
-  name: 'App',
-  components: {
-    JobsC
-  }
+	components: {
+		JobListing
+	},
+	data() {
+		return {
+			jobs: [
+				{
+					company: "Genuine Digital School",
+					icon: "https://s3-us-west-2.amazonaws.com/peaku-public/business/Company/logo_url/530708/Logo-Genuine-Digital-School.jpg",
+					name: "Profesor De Matemáticas",
+					location: "Remoto - Cualquier Lugar",
+					salary: "Confidencial",
+					vacants: 1,
+					creationDate: new Date(2004, 2, 22),
+					requirements: ["Microsoft 365", "English C1", "Pedagogical Methodology"]
+				}
+			]
+		}
+	}
 }
 </script>
 
