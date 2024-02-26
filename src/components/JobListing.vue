d46d3207e018e948c6f93a7085f6be7e5de8dc75
Andres Caicedo <andrezz1997@gmail.com>
2024-02-26 11:34:35 +0100

    fix: Rename JobListing component with required props and styles

--- src/components/JobListing.vue
+++ src/components/JobListing.vue
@@ -31,11 +31,11 @@
 </template>
 
 <script>
-import RequirementC from "./RequirementC.vue";
-import DetailsC from "./DetailsC.vue";
+import RequirementC from "./JobRequirement.vue";
+import DetailsC from "./JobDetails.vue";
 
 export default {
-	name: "JobC",
+	name: "JobListing",
 	components: {
 		RequirementC,
 		DetailsC,
@@ -52,41 +52,6 @@ export default {
 	},
 };
 </script>
-
-<style scoped>
-#main-content {
-	align-items: center;
-	background-color: var(--secondary-color);
-	border-color: var(--primary-color-color);
-	border-width: 10px;
-	color: var(--primary-color-color);
-	display: flex;
-	flex-direction: row;
-	justify-content: start;
-	margin: 15px;
-	padding: 5px;
-	transition: box-shadow 0.2s ease-in-out;
-}
-
-#main-content:hover {
-	box-shadow: 10px 10px #ffffff50;
-}
-
-#icon {
-	margin: 10px;
-}
-
-#container {
-	display: flex;
-	margin: 10px;
-	flex-direction: column;
-}
-
-#name-and-requirements {
-	display: flex;
-	margin: 5px;
-}
-</style>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
