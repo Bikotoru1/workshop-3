cce5518b33bf65b9066291c8ea22800829a1080a
Andres Caicedo <andrezz1997@gmail.com>
2024-03-03 15:32:59 +0100

    Refactor component styles and formatting

--- src/components/JobMainContent.vue
+++ src/components/JobMainContent.vue
@@ -4,21 +4,48 @@
       <div class="card-body p-3" id="job-card-body">
         <div class="row align-items-center">
           <div class="col-md-2 col-4 text-center">
-            <img alt="logo empresa" class="rounded img-fluid" id="icon" height="70" :src="icon" width="70" />
+            <img
+              alt="logo empresa"
+              class="rounded img-fluid"
+              id="icon"
+              height="70"
+              :src="icon"
+              width="70"
+            />
           </div>
 
           <div class="col-md-10 col-8">
-            <div class="d-flex flex-wrap justify-content-between align-items-center mb-2">
-              <a class="text-dark d-flex flex-wrap text-decoration-none" href="#">
-                <h2 class="font-weight-bold" itemprop="title" id="job-title">{{ name }}</h2>
+            <div
+              class="d-flex flex-wrap justify-content-between align-items-center mb-2"
+            >
+              <a
+                class="text-dark d-flex flex-wrap text-decoration-none"
+                href="#"
+              >
+                <h2 class="font-weight-bold" itemprop="title" id="job-title">
+                  {{ name }}
+                </h2>
 
-                <section class="pl-2 d-flex flex-wrap align-items-center" itemprop="skills">
-                  <JobRequirement v-for="(requirement, index) in requirements" :key="index" :text="requirement" />
+                <section
+                  class="pl-2 d-flex flex-wrap align-items-center"
+                  itemprop="skills"
+                >
+                  <JobRequirement
+                    v-for="(requirement, index) in requirements"
+                    :key="index"
+                    :text="requirement"
+                  />
                 </section>
               </a>
             </div>
 
-            <JobDetails :company="company" :location="location" :salary="salary" :vacancies="vacancies" :creation-date="creationDate" />
+            <JobDetails
+              :company="company"
+              :location="location"
+              :salary="salary"
+              :vacancies="vacancies"
+              :creation-date="creationDate"
+            />
           </div>
         </div>
       </div>
@@ -27,76 +54,76 @@
 </template>
 
 <script>
-import JobRequirement from "./JobRequirement.vue"
-import JobDetails from "./JobDetails.vue"
+import JobRequirement from "./JobRequirement.vue";
+import JobDetails from "./JobDetails.vue";
 
 export default {
-	name: 'JobMainContent',
-	components: {
-		JobRequirement,
-		JobDetails
-	},
-	props: {
-		icon: String,
-		name: String,
-		company: String,
-		location: String,
-		salary: String,
-		vacancies: Number,
-		creationDate: Date,
-		requirements: [],
-	}
-}
+  name: "JobMainContent",
+  components: {
+    JobRequirement,
+    JobDetails,
+  },
+  props: {
+    icon: String,
+    name: String,
+    company: String,
+    location: String,
+    salary: String,
+    vacancies: Number,
+    creationDate: Date,
+    requirements: [],
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 #job-listing {
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	transition: all 0.3s ease-in-out;
-	width: calc(100% - 1rem) !important;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all 0.3s ease-in-out;
+  width: calc(100% - 1rem) !important;
 }
 
 #job-card {
-	margin: auto;
-	transition: all 0.3s ease-in-out;
-	/* Relative width */
-	width: calc(100% - 1rem);
+  margin: auto;
+  transition: all 0.3s ease-in-out;
+  /* Relative width */
+  width: calc(100% - 1rem);
 }
 
 #job-card-body {
-	transition: all 0.3s ease-in-out;
+  transition: all 0.3s ease-in-out;
 }
 
 #job-card-body:hover {
-		box-shadow: 5px 5px 5px var( --secondary-color );
+  box-shadow: 5px 5px 5px var(--secondary-color);
 }
 
 #job-title {
-	margin: auto;
-	font-size: 1.1rem;
-	padding: 5px;
-	transition: all 0.3s ease-in-out;
-	text-decoration: none;
-	color: var(--tertiary-color);
+  margin: auto;
+  font-size: 1.1rem;
+  padding: 5px;
+  transition: all 0.3s ease-in-out;
+  text-decoration: none;
+  color: var(--tertiary-color);
 }
 
 #job-title:hover {
-	font-size: 1.15rem !important;
-	color: var(--primary-color) !important;
+  font-size: 1.15rem !important;
+  color: var(--primary-color) !important;
 }
 
 .bold,
 .bolder,
 b {
-	font-family: "Montserrat", "Roboto", sans-serif;
+  font-family: "Montserrat", "Roboto", sans-serif;
 }
 
 .bold,
 b {
-	font-weight: 600 !important;
-	-webkit-font-smoothing: antialiased;
+  font-weight: 600 !important;
+  -webkit-font-smoothing: antialiased;
 }
 </style>
--- src/components/JobRequirement.vue
+++ src/components/JobRequirement.vue
@@ -1,48 +1,51 @@
 <template>
-	<div class="skill-tag text-truncate justify-content-evenly"	id="content" role="button">
-		<span>{{ text }}</span>
-	</div>
+  <div
+    class="skill-tag text-truncate justify-content-evenly"
+    id="content"
+    role="button"
+  >
+    <span>{{ text }}</span>
+  </div>
 </template>
 
 <script>
 export default {
-	name: 'JobRequirement',
-	props: {
-		text: String
-	}
-}
+  name: "JobRequirement",
+  props: {
+    text: String,
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-	#content {
-		justify-content: space-evenly;
-		align-items: center;
-		text-align: center;
-		font-size: 0.7em;
-		margin:  0.5rem 0.5rem 0.5rem 0;
-		padding: 0.2rem 0.5rem;
+#content {
+  justify-content: space-evenly;
+  align-items: center;
+  text-align: center;
+  font-size: 0.7em;
+  margin: 0.5rem 0.5rem 0.5rem 0;
+  padding: 0.2rem 0.5rem;
+}
 
-	}
+.skill-tag {
+  color: var(--tertiary-color);
+  padding: 0.2rem 0.5rem;
+  border-radius: 0.5rem;
+  border: 1px solid var(--tertiary-color);
+  transition: all 0.3s ease-in-out;
+  margin: 0.5rem 0.5rem 0.5rem 0;
+}
 
-	.skill-tag {
-		color: var(--tertiary-color);
-		padding: 0.2rem 0.5rem;
-		border-radius: 0.5rem;
-		border: 1px solid var(--tertiary-color);
-		transition: all 0.3s ease-in-out;
-		margin: 0.5rem 0.5rem 0.5rem 0;
-	}
+.skill-tag:hover {
+  box-shadow: 0 0 5px 0 var(--tertiary-color);
+  border: 1px solid var(--primary-color);
+  color: var(--primary-color);
+  cursor: pointer;
+}
 
-	.skill-tag:hover {
-		box-shadow: 0 0 5px 0 var(--tertiary-color);
-		border: 1px solid var(--primary-color);
-		color: var(--primary-color);
-		cursor: pointer;
-	}
-
-	h2,
-	p {
-		margin: auto;
-	}
+h2,
+p {
+  margin: auto;
+}
 </style>
